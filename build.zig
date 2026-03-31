c7d3d0530d1dad5308ea2dd2d73ce32a5d86420a
Mitchell <mgerb42@gmail.com>
2026-03-30 20:48:08 -0500

    chore: upgrade to pipewire 1.6.2

--- build.zig
+++ build.zig
@@ -289,14 +289,14 @@ pub fn build(b: *std.Build) void {
                 //         .flags = flags,
                 //     });
                 // }
-                if (target.result.cpu.has(.x86, .avx) and target.result.cpu.has(.x86, .fma)) {
+                if (target.result.cpu.has(.x86, .avx2) and target.result.cpu.has(.x86, .fma)) {
                     // Upstream build system also only defines either if both are present
-                    audioconvert.root_module.addCMacro("HAVE_AVX", "1");
+                    audioconvert.root_module.addCMacro("HAVE_AVX2", "1");
                     audioconvert.root_module.addCMacro("HAVE_FMA", "1");
                     audioconvert.root_module.addCSourceFiles(.{
                         .root = upstream.path("spa/plugins/audioconvert"),
                         .files = &.{
-                            "resample-native-avx.c",
+                            "resample-native-avx2.c",
                         },
                         .flags = flags,
                     });
